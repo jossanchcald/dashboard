aceb64a11dc195ff1601e76033fc0b5997fec377
jossanchcald <jossanch@espol.edu.ec>
2026-07-06 14:04:32 -0500

    cambios minimos sintaxis App e instalacion paquete de despliegue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import { Grid } from '@mui/material';
 
@@ -10,10 +7,10 @@ function App() {
     <Grid container spacing={5} sx={{ justifyContent: "left", alignItems: "center" }}>
 
       {/* Encabezado */}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+      <Grid size={12}>Elemento: Encabezado</Grid>
 
       {/* Alertas */}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+      <Grid size={12}>Elemento: Alertas</Grid>
 
       {/* Selector */}
       <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
@@ -28,7 +25,7 @@ function App() {
       <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
 
       {/* Información adicional */}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+      <Grid size={12}>Elemento: Información adicional</Grid>
 
     </Grid>
   );
