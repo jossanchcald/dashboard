4043db63c11b8ec9ea7fc7773999fe349069ed4a
jossanchcald <jossanch@espol.edu.ec>
2026-07-06 14:50:01 -0500

    Creados componentes HeaderUI y AlertUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css'
-import { Grid } from '@mui/material';
+import { Alert, Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 
 function App() {
   return (
     <Grid container spacing={5} sx={{ justifyContent: "left", alignItems: "center" }}>
 
       {/* Encabezado */}
-      <Grid size={12}>Elemento: Encabezado</Grid>
+      <Grid size={12}>
+        <HeaderUI/>
+      </Grid>
 
       {/* Alertas */}
-      <Grid size={12}>Elemento: Alertas</Grid>
+      <Grid size={12} container sx={{ justifyContent: "right", alignItems: "center" }}>
+        <AlertUI description="No se preveen lluvias"/>
+      </Grid>
 
       {/* Selector */}
       <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
