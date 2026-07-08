8fb78afce496aad5ce000c6c4aee3a3255559b8a
jossanchcald <jossanch@espol.edu.ec>
2026-07-08 14:06:52 -0500

    Modificado elemento SelectorUI agregando un hook useState

--- src/components/SelectorUI.tsx
+++ src/components/SelectorUI.tsx
@@ -2,11 +2,14 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select, { type SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react';
 
 export default function SelectorUI() {
 
+    const [cityInput, setCityInput] = useState("");
+    
     const handleChange = (event: SelectChangeEvent<string>) => {
-        alert(event.target.value);
+        setCityInput(event.target.value);
     };
 
     return (
@@ -16,14 +19,20 @@ export default function SelectorUI() {
                 labelId="city-select-label"
                 id="city-simple-select"
                 label="Ciudad"
-                onChange={handleChange}>
+                onChange={handleChange}
+                value={cityInput}>
                 <MenuItem disabled><em>Seleccione una ciudad</em></MenuItem>
                 <MenuItem value={"guayaquil"}>Guayaquil</MenuItem>
                 <MenuItem value={"quito"}>Quito</MenuItem>
                 <MenuItem value={"manta"}>Manta</MenuItem>
                 <MenuItem value={"cuenca"}>Cuenca</MenuItem>
             </Select>
-
+            
+                {cityInput && (
+                    <p>
+                        Información del clima en <span style={{textTransform: 'capitalize', fontWeight: 'bold'}}>{cityInput}</span>
+                    </p>
+                )}
         </FormControl>
     )
 }
