4b9f280033563ab663fa8d73619a2af27a499e21
jossanchcald <jossanch@espol.edu.ec>
2026-07-13 13:49:22 -0500

    Creacion elemento IndicatorUI y estructura base de datos a recibir de la API OpenMeteo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Grid } from '@mui/material';
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
 import SelectorUI from './components/SelectorUI';
+import IndicatorUI from './components/IndicatorUI';
 
 function App() {
   return (
@@ -24,7 +25,25 @@ function App() {
       </Grid>
 
       {/* Indicadores */}
-      <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+      <Grid container size={{ xs: 12, md: 9 }} >
+
+        <Grid size={{ xs: 12, md: 3 }}>
+          <IndicatorUI title='Temperatura (2m)' description='XX °C' />
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 3 }}>
+        <IndicatorUI title='Temperatura Aparente' description='XX °C' />
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 3 }}>
+        <IndicatorUI title='Velocidad del viento (10m)' description='XX km/h' />
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 3 }}>
+        <IndicatorUI title='Humedad Relativa (2m)' description='XX %' />
+        </Grid>
+
+      </Grid>
 
       {/* Gráfico */}
       <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Gráfico</Grid>
