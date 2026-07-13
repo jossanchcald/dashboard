69c6a6caf773d4d57cec51b027ad8133f053db17
jossanchcald <jossanch@espol.edu.ec>
2026-07-13 16:51:29 -0500

    Cambio de fetch de una ciudad a varias definidas por la lista previa en SelectorUI, con forma base para mostrar datos de varias ciudades, pendiente de revision

--- src/components/SelectorUI.tsx
+++ src/components/SelectorUI.tsx
@@ -4,35 +4,43 @@ import Select, { type SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import { useState } from 'react';
 
-export default function SelectorUI() {
+interface SelectorUIProps {
+    value: string;
+    onChange: (value: string) => void;
+}
 
-    const [cityInput, setCityInput] = useState("");
-    
+export default function SelectorUI({
+    value,
+    onChange
+}: SelectorUIProps) {
+
     const handleChange = (event: SelectChangeEvent<string>) => {
-        setCityInput(event.target.value);
+        onChange(event.target.value);
     };
 
     return (
         <FormControl fullWidth>
             <InputLabel id="city-select-label">Ciudad</InputLabel>
+
             <Select
                 labelId="city-select-label"
-                id="city-simple-select"
                 label="Ciudad"
+                value={value}
                 onChange={handleChange}
-                value={cityInput}>
-                <MenuItem disabled><em>Seleccione una ciudad</em></MenuItem>
-                <MenuItem value={"guayaquil"}>Guayaquil</MenuItem>
-                <MenuItem value={"quito"}>Quito</MenuItem>
-                <MenuItem value={"manta"}>Manta</MenuItem>
-                <MenuItem value={"cuenca"}>Cuenca</MenuItem>
+            >
+                <MenuItem value="guayaquil">Guayaquil</MenuItem>
+                <MenuItem value="quito">Quito</MenuItem>
+                <MenuItem value="manta">Manta</MenuItem>
+                <MenuItem value="cuenca">Cuenca</MenuItem>
             </Select>
-            
-                {cityInput && (
-                    <p>
-                        Información del clima en <span style={{textTransform: 'capitalize', fontWeight: 'bold'}}>{cityInput}</span>
-                    </p>
-                )}
+
+            <p>
+                Información del clima en{" "}
+                <span style={{ textTransform: "capitalize", fontWeight: "bold" }}>
+                    {value}
+                </span>
+            </p>
+
         </FormControl>
-    )
+    );
 }
